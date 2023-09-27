8ac9905739936d25154e7d809877aec5ccd8da0a
ShanThatos <shanthkoka@gmail.com>
2023-09-27 02:20:49 -0500

    removed base for new domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/vrdemo/",
     server: {
         host: "0.0.0.0",
         https: true
